70ae99fbadf51d72c41d7b4ae070416c6b5cb775
Paul Clark <clark.paul123@gmail.com>
2018-09-19 17:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ function placeOrder(cardNumber) {
   } else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`; 
   }
-  return cart = 0; 
+  return cart.length = 0; 
 }
 
