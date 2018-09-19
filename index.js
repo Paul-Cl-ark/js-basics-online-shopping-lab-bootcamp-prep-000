f5a64e534d8830d28e310942a3f8a6dd48bbc8d6
Paul Clark <clark.paul123@gmail.com>
2018-09-19 17:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ let cart = getCart();
 }
 
 function placeOrder(cardNumber) {
-  function empty()
+  function empty() 
   if (!cardNumber) {
   return "Sorry, we don't have a credit card on file for you.";
   } else {
